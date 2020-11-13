bf23fb94697a4d46edc370df0089c6c13378e483
Sarah <sarahchloe.chakra@gmail.com>
2020-11-13 22:39:35 +0100

    Add random index generator

--- message-generator.js
+++ message-generator.js
@@ -7,3 +7,13 @@ let verbs = ['has', 'is', 'bakes', 'dearly loves', 'kisses', 'was', 'has', 'sham
 let complements = ['amazing DPS scores', 'disgusting space cookies', 'Britney Spears', 'the song Oops I did it again', 'Assassins Creeds life hacks', 'motherlode cheat codes to get a ferrari in real life', 'my proudest moments', 'an oversensitive camper on Call of Duty', 'a lazy warlock', 'a magnificent painting exhibition'];
 
 let complements1 = ['while watching french TV reality shows', 'while scolding Hermione and Rons kids', 'during a way too long conference call', 'because thats what being fabulous is about', 'in Bruges for a well-deserved holiday', 'because being a vegan is way too hard', 'in order to quit smoking autumn leaves', 'since french fries are not really french', 'to replace the healer thats not doing their job'];
+
+// Now, we will generate a random whole number, which we will use as our index for our previous arrays of strings (subject, verbs and complements)
+
+const generateRandomIndex = array => {
+    let randomIndex = Math.floor(Math.random() * array.length);
+    return randomIndex;
+};
+
+console.log('Running script seems to be working')
+console.log(generateRandomIndex(verbs));
